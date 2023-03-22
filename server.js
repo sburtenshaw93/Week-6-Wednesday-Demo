8bdd47ca41629a129356367b74d1101bac181bda
Sarah Burtenshaw <sarahburtenshaw@Sarahs-Air.localdomain>
2023-03-22 12:26:32 -0600

    Now serves front end files from back end endpoints

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(__dirname + '/public/styles.css')
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(__dirname + '/public/main.js')
+})
+
 app.listen(5150, () => {
     console.log('app is up on 5150')
     })
